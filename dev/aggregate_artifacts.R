a23368eefafb7ed7366de403e1ec80b6ed0d1078
Eduardo Azevedo <eduaze@gmail.com>
2026-02-11 18:52:49 -0500

    better artifacts

--- dev/aggregate_artifacts.R
+++ dev/aggregate_artifacts.R
@@ -23,24 +23,32 @@ aggregate_coverage_artifacts <- function(
   }
 
   parse_scenario <- function(scenario) {
-    result <- list(model_type = NA_character_, n = NA_integer_, var_epsilon = NA_real_, log_k = NA_real_)
+    result <- list(
+      model_type = NA_character_,
+      n = NA_integer_,
+      var_epsilon = NA_real_,
+      var_v_factor = NA_real_,
+      log_k = NA_real_
+    )
 
-    coverage_match <- regexec("^(lm|probit)_n([0-9]+)_ve([0-9.]+)$", scenario)
+    coverage_match <- regexec("^(lm|probit)_n([0-9]+)_ve([0-9.]+)_vv([0-9.]+)$", scenario)
     coverage_parts <- regmatches(scenario, coverage_match)[[1]]
     if (length(coverage_parts) > 0) {
       result$model_type <- coverage_parts[2]
       result$n <- as.integer(coverage_parts[3])
       result$var_epsilon <- as.numeric(coverage_parts[4])
+      result$var_v_factor <- as.numeric(coverage_parts[5])
       return(result)
     }
 
-    survival_match <- regexec("^(exp|wei)_n([0-9]+)_ve([0-9.]+)_lk(-?[0-9.]+)$", scenario)
+    survival_match <- regexec("^(exp|wei)_n([0-9]+)_ve([0-9.]+)_vv([0-9.]+)_lk(-?[0-9.]+)$", scenario)
     survival_parts <- regmatches(scenario, survival_match)[[1]]
     if (length(survival_parts) > 0) {
       result$model_type <- survival_parts[2]
       result$n <- as.integer(survival_parts[3])
       result$var_epsilon <- as.numeric(survival_parts[4])
-      result$log_k <- as.numeric(survival_parts[5])
+      result$var_v_factor <- as.numeric(survival_parts[5])
+      result$log_k <- as.numeric(survival_parts[6])
     }
 
     result
@@ -58,6 +66,7 @@ aggregate_coverage_artifacts <- function(
       model_type = meta$model_type,
       n = meta$n,
       var_epsilon = meta$var_epsilon,
+      var_v_factor = meta$var_v_factor,
       log_k = meta$log_k,
       stringsAsFactors = FALSE
     )
